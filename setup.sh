d7b047174c152cf1fbb77a73294a34902124253d
Harsh Pala <harshpala1@gmail.com>
2024-11-28 05:35:18 +0530

    updated setup file

--- setup.sh
+++ setup.sh
@@ -9,10 +9,12 @@ mkdir -p "$PROJECT_ROOT/include/core" "$PROJECT_ROOT/include/protocols"
 
 # Create empty source files in respective subfolders
 [ ! -f "$PROJECT_ROOT/src/core/main.cpp" ] && touch "$PROJECT_ROOT/src/core/main.cpp"
+[ ! -f "$PROJECT_ROOT/src/core/User.cpp" ] && touch "$PROJECT_ROOT/src/core/User.cpp"
 [ ! -f "$PROJECT_ROOT/src/core/Simulator.cpp" ] && touch "$PROJECT_ROOT/src/core/Simulator.cpp"
 [ ! -f "$PROJECT_ROOT/src/protocols/WiFi4Simulator.cpp" ] && touch "$PROJECT_ROOT/src/protocols/WiFi4Simulator.cpp"
 
 # Create empty header files in respective subfolders
+[ ! -f "$PROJECT_ROOT/include/core/User.h" ] && touch "$PROJECT_ROOT/include/core/User.h"
 [ ! -f "$PROJECT_ROOT/include/core/Simulator.h" ] && touch "$PROJECT_ROOT/include/core/Simulator.h"
 [ ! -f "$PROJECT_ROOT/include/protocols/WiFi4Simulator.h" ] && touch "$PROJECT_ROOT/include/protocols/WiFi4Simulator.h"
 
@@ -26,15 +28,21 @@ echo "
 $PROJECT_ROOT/
 │
 ├── src/
+│   │
 │   ├── core/
 │   │   ├── main.cpp        # Main application entry
 │   │   ├── Simulator.cpp   # Simulator base class implementation
+│   │   └── User.cpp        # User class implementation
+│   │   
 │   └── protocols/
 │       └── WiFi4Simulator.cpp  # WiFi 4 Simulator implementation
 │
 └── include/
+    │
     ├── core/
-    │   └── Simulator.h     # Simulator base class header
+    │    ├──Simulator.h     # Simulator base class header
+    │    └──User.h          # User class header
+    │
     └── protocols/
-        └── WiFi4Simulator.h  # WiFi 4 Simulator header
+         └── WiFi4Simulator.h  # WiFi 4 Simulator header
 "
